ccbd904f5d55dea56d900dde0536d590a64b1f89
gernest <geofreyernest@live.com>
2018-10-22 08:17:35 +0300

    add comments

--- src/image/color.zig
+++ src/image/color.zig
@@ -1,7 +1,16 @@
+/// Color can convert itself to alpha-premultiplied 16-bits per channel RGBA.
+/// The conversion may be lossy.
 pub const Color = struct.{
     rgb: fn () Value,
 };
 
+/// Value is the alpha-premultiplied red, green, blue and alpha values
+/// for the color. Each value ranges within [0, 0xffff], but is represented
+/// by a uint32 so that multiplying by a blend factor up to 0xffff will not
+/// overflow.
+///
+/// An alpha-premultiplied color component c has been scaled by alpha (a),
+/// so has valid values 0 <= c <= a.
 pub const Value = struct.{
     r: u32,
     g: u32,
