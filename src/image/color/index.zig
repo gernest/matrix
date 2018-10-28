b18f8e8bd844d70f2134b84b881486093f131517
gernest <geofreyernest@live.com>
2018-10-28 11:20:34 +0300

    complete color models

--- src/image/color/index.zig
+++ src/image/color/index.zig
@@ -68,6 +68,10 @@ pub const RGBA64 = struct.{
     a: u16,
 
     fn toColor(c: RGBA64) Value {
+        return Color.{ .rgb = c.toValue };
+    }
+
+    fn toValue(c: RGBA64) Value {
         return Value.{
             .r = c.r,
             .g = c.g,
@@ -204,7 +208,13 @@ pub const Gray16 = struct.{
     }
 };
 
-pub const Alpha16Model = Model.{ .convert = ModelType.rgbaModel };
+pub const RGBAModel = Model.{ .convert = ModelType.rgbaModel };
+pub const RGBA64Model = Model.{ .convert = ModelType.rgba64Model };
+pub const NRGBAModel = Model.{ .convert = ModelType.nrgbaModel };
+pub const NRGBA64Model = Model.{ .convert = ModelType.nrgba64Model };
+pub const Alpha16Model = Model.{ .convert = ModelType.alpha16Model };
+pub const GrayModel = Model.{ .convert = ModelType.grayModel };
+pub const Gray16Model = Model.{ .convert = ModelType.gray16Model };
 
 pub const ModelType = union(enum).{
     rgba: RGBA,
@@ -221,14 +231,162 @@ pub const ModelType = union(enum).{
         return switch (m) {
             ModelType.rgba => |c| c.toColor(),
             ModelType.color => |c| {
-                return Color.{
-                    .rgb = Value.{
-                        .r = c.rgb.r >> 8,
-                        .g = c.rgb.g >> 8,
-                        .b = c.rgb.b >> 8,
-                        .a = c.rgb.a >> 8,
-                    },
+                const model = RGBA.{
+                    .r = @intCast(u8, c.rgb.r >> 8),
+                    .g = @intCast(u8, c.rgb.g >> 8),
+                    .b = @intCast(u8, c.rgb.b >> 8),
+                    .a = @intCast(u8, c.rgb.a >> 8),
                 };
+                return model.toColor();
+            },
+            else => unreachable,
+        };
+    }
+
+    pub fn rgba64Model(m: ModelType) Color {
+        return switch (m) {
+            ModelType.rgba64 => |c| c.toColor(),
+            ModelType.color => |c| {
+                const model = RGBA64.{
+                    .r = c.rgb.r,
+                    .g = c.rgb.g,
+                    .b = c.rgb.b,
+                    .a = c.rgb.a,
+                };
+                return model.toColor();
+            },
+            else => unreachable,
+        };
+    }
+
+    pub fn nrgbaModel(m: ModelType) Color {
+        return switch (m) {
+            ModelType.nrgba => |c| c.toColor(),
+            ModelType.color => |c| {
+                if (c.rgb.a == 0xffff) {
+                    const model = NRGBA.{
+                        .r = @intCast(u8, c.rgb.r >> 8),
+                        .g = @intCast(u8, c.rgb.g >> 8),
+                        .b = @intCast(u8, c.rgb.b >> 8),
+                        .a = 0xff,
+                    };
+                    return model.toColor();
+                }
+                if (c.rgb.a == 0) {
+                    const model = NRGBA.{
+                        .r = 0,
+                        .g = 0,
+                        .b = 0,
+                        .a = 0,
+                    };
+                    return model.toColor();
+                }
+                var r = (c.rgb.r * 0xffff) / c.rgb.a;
+                var g = (c.rgb.g * 0xffff) / c.rgb.a;
+                var b = (c.rgb.b * 0xffff) / c.rgb.a;
+                const model = NRGBA.{
+                    .r = @intCast(u8, r >> 8),
+                    .g = @intCast(u8, g >> 8),
+                    .b = @intCast(u8, b >> 8),
+                    .a = @intCast(u8, c.rgb.a >> 8),
+                };
+                return model.toColor();
+            },
+            else => unreachable,
+        };
+    }
+    pub fn nrgba64Model(m: ModelType) Color {
+        return switch (m) {
+            ModelType.nrgba64 => |c| c.toColor(),
+            ModelType.color => |c| {
+                if (c.rgb.a == 0xffff) {
+                    const model = NRGBA64.{
+                        .r = c.rgb.r,
+                        .g = c.rgb.g,
+                        .b = c.rgb.b,
+                        .a = 0xff,
+                    };
+                    return model.toColor();
+                }
+                if (c.rgb.a == 0) {
+                    const model = NRGBA64.{
+                        .r = 0,
+                        .g = 0,
+                        .b = 0,
+                        .a = 0,
+                    };
+                    return model.toColor();
+                }
+                var r = (c.rgb.r * 0xffff) / c.rgb.a;
+                var g = (c.rgb.g * 0xffff) / c.rgb.a;
+                var b = (c.rgb.b * 0xffff) / c.rgb.a;
+                const model = NRGBA64.{
+                    .r = r,
+                    .g = g,
+                    .b = b,
+                    .a = c.rgb.a,
+                };
+                return model.toColor();
+            },
+            else => unreachable,
+        };
+    }
+
+    pub fn alphaModel(m: ModelType) Color {
+        return switch (m) {
+            ModelType.alpha => |c| c.toColor(),
+            ModelType.color => |c| {
+                const model = Alpha.{ .a = @intCast(u8, c.rgb.a >> 8) };
+                return model.toColor();
+            },
+            else => unreachable,
+        };
+    }
+
+    pub fn alpha16Model(m: ModelType) Color {
+        return switch (m) {
+            ModelType.alpha16 => |c| c.toColor(),
+            ModelType.color => |c| {
+                const model = Alpha16.{ .a = @intCast(u16, c.rgb.a) };
+                return model.toColor();
+            },
+            else => unreachable,
+        };
+    }
+
+    pub fn grayModel(m: ModelType) Color {
+        return switch (m) {
+            ModelType.gray => |c| c.toColor(),
+            ModelType.color => |c| {
+
+                // These coefficients (the fractions 0.299, 0.587 and 0.114) are the same
+                // as those given by the JFIF specification and used by func RGBToYCbCr in
+                // ycbcr.go.
+                //
+                // Note that 19595 + 38470 + 7471 equals 65536.
+                //
+                // The 24 is 16 + 8. The 16 is the same as used in RGBToYCbCr. The 8 is
+                // because the return value is 8 bit color, not 16 bit color.
+                const y = (19595 * c.rgb.r + 38470 * c.rgb.g + 7471 * c.rgb.b + 1 << 15) >> 24;
+                const model = Gray.{ .y = @intCast(u8, y) };
+                return model.toColor();
+            },
+            else => unreachable,
+        };
+    }
+    pub fn gray16Model(m: ModelType) Color {
+        return switch (m) {
+            ModelType.gray16 => |c| c.toColor(),
+            ModelType.color => |c| {
+
+                // These coefficients (the fractions 0.299, 0.587 and 0.114) are the same
+                // as those given by the JFIF specification and used by func RGBToYCbCr in
+                // ycbcr.go.
+                //
+                // Note that 19595 + 38470 + 7471 equals 65536.
+                const y = (19595 * c.rgb.r + 38470 * c.rgb.g + 7471 * c.rgb.b + 1 << 15) >> 16;
+                const model = Gray16.{ .y = @intCast(u16, y) };
+                return model.toColor();
             },
             else => unreachable,
         };
